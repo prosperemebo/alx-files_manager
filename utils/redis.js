11b5f2f7d6fa5073304c7c6aef0f3c38699511bf
prosperemebo <emeboprosper@gmail.com>
2024-06-17 01:13:26 +0100

    Task 0

---
+++ utils/redis.js
@@ -0,0 +1,40 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = false;
+
+    this.client.on('error', (err) => console.log(err));
+    this.client.on('ready', () => {
+      this.isConnected = true;
+    });
+
+    this.Get = promisify(this.client.get).bins(this.client);
+    this.SetExp = promisify(this.client.set).bind(this.client);
+    this.Del = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return this.Get(key).then((value) => value);
+  }
+
+  async set(key, value, duration) {
+    await this.SetExp(key, value);
+
+    if (duration) {
+      await this.client.expire(key, duration);
+    }
+  }
+
+  async del(key) {
+    await this.Del(key);
+  }
+}
+
+export default new RedisClient();
